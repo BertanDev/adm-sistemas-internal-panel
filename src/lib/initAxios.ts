0c2fd88fe9e68a0c95a98f1331d66821ffaeaabd
João Vitor Bertan <72395637+BertanDev@users.noreply.github.com>
2024-03-26 09:26:48 -0300

    aws

--- src/lib/initAxios.ts
+++ src/lib/initAxios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  // baseURL: 'http://54.207.219.196:8080',
+  // baseURL: 'http://localhost:8080',
+  baseURL: 'http://18.229.226.244:8080',
 })
 
 export { api }
